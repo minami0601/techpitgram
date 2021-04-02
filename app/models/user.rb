9f3b6cee24c03a0c95e92aa2e06ecd8921f81348
minami0601 <minami.love.bb@icloud.com>
2021-04-02 21:54:27 +0900

    ✨ ニックネーム追加機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true, length: { maximum: 50 }
+  validates :username, uniqueness: true, presence: true, length: { maximum: 50 }
 
   def update_without_current_password(params, *options)
 
